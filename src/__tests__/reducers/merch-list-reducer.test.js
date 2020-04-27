7e325ecfa4e0541094550706f7d877891248c10f
Joseph Wangemann <fractalscape13@gmail.com>
2020-04-27 15:00:34 -0700

    add test directory and 3 tests for merch-list-reducer
    Co-authored-by: acolistro <arcolistro@gmail.com>
    Co-authored-by: K. Wicz <katy@solovewicz.com>

---
+++ src/__tests__/reducers/merch-list-reducer.test.js
@@ -0,0 +1,63 @@
+import merchListReducer from '../../reducers/merch-list-reducer';
+
+describe('merchListReducer', () => {
+
+  let action;
+
+  const currentState = {
+    1: {name: 'Alyssa, Joseph, & Katy',
+      description: 'Reduxalated and Reactivated',
+      quantity: 420,
+      id: 1 },
+    2: {name: 'Other people',
+      description: 'Reduxified',
+      quantity: 421,
+      id: 2 }
+  }
+
+  const merchData = {
+    name: 'Alyssa, Joseph, & Katy',
+    description: 'Reduxalated and Reactivated',
+    quantity: 420,
+    id: 1
+  };
+
+  test('Should return default state if there is no action type passed into the reducer', () => {
+    expect(merchListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully add new merchdandise data to masterMerchList', () => {
+    const { name, description, quantity, id } = merchData;
+    action = {
+      type: 'ADD_MERCH',
+      name: name,
+      description: description,
+      quantity: quantity,
+      id: id
+    };
+    
+    expect(merchListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        description: description,
+        quantity: quantity,
+        id: id
+      }
+    });
+  });
+
+  test('Should successfully delete a merch item', () => {
+    action = {
+      type: 'DELETE_MERCH',
+      id: 1
+    };
+    expect(merchListReducer(currentState, action)).toEqual({
+      2: {name: 'Other people',
+      description: 'Reduxified',
+      quantity: 421,
+      id: 2 }
+    });
+  });
+
+
+});
